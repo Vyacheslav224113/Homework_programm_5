72abb0cdca8c40b2e2c8f6861e9bc337a569b463
Slava Korolev <vyacheslav.korolev.ru@mail.ru>
2024-02-25 19:33:55 +0700

    Homework

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Создание строки из двумерного массива символов
+
+// public class Task1
+// {
+//     public static void Main(string[] args)
+//     {
+//         char[,] charArray = new char[,] { { 'a', 'b' }, { 'c', 'd' } };
+//         string result = CreateStringFrom2DArray(charArray);
+//         Console.WriteLine(result);
+//     }
+//     public static string CreateStringFrom2DArray(char[,] array)
+//     {
+//         string result = "";
+//         for (int i = 0; i < array.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//             {
+//                 result += array[i, j];
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+
+// Задача 2: Замена заглавных букв на строчные в строке
+
+// public class Task2
+// {
+//     public static void Main(string[] args)
+//     {
+//         string input = "aBcD1ef!-";
+//         string result = input.ToLower();
+//         Console.WriteLine(result);
+//     }
+// }
+
+// Задача 3: Проверка строки на палиндром
+// public class Task3
+// {
+//     public static void Main(string[] args)
+//     {
+//         string input = "шалаш";
+//         bool isPalindrome = IsPalindrome(input);
+//         Console.WriteLine(isPalindrome ? "Да" : "Нет");
+//     }
+// public static bool IsPalindrome(string str)
+// {
+// string normalized = new
+// string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+// return normalized.SequenceEqual(normalized.Reverse());
+// }
+// }
